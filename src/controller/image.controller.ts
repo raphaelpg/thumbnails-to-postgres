fe7f71ccbbba861a95d882c8b46a304f739581b7
raphaelpg <raphael_pinto@outlook.com>
2021-04-20 17:53:17 +0100

    temporary remove ts error

--- src/controller/image.controller.ts
+++ src/controller/image.controller.ts
@@ -4,21 +4,21 @@ import imageServices from '../service/image.service';
 const saveImage = (req: Request, res: Response) => {
   if (true) {
     imageServices.saveImage()
-    return res.status(200).json({msg: "posted image saved"})
+    return res.status(200).json({ msg: "posted image saved", paramaters: req.params })
   }
 };
 
 const getImageById = (req: Request, res: Response) => {
   if (true) {
     imageServices.getImageById();
-    return res.status(200).json({msg: "get image by id", id: req.params})
+    return res.status(200).json({ msg: "get image by id", id: req.params })
   }
 };
 
 const getAllImages = (req: Request, res: Response) => {
   if (true) {
     imageServices.getImages();
-    return res.status(200).json({msg: "get all images"})
+    return res.status(200).json({ msg: "get all images", parameters: req.params })
   }
 };
 
