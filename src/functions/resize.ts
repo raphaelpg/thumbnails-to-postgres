6df9c2069d7bf9d7a0aa5b750a813ef41509423b
raphaelpg <raphael_pinto@outlook.com>
2021-04-22 16:15:30 +0100

    factor sharp function

---
+++ src/functions/resize.ts
@@ -0,0 +1,14 @@
+import sharp from 'sharp';
+import path from 'path';
+
+const resizeImage = (file: any, width: number, height: number) => {
+  const outputPath = './build/src/public/thumbnails/' + path.parse(file.originalname).name + `-resized-${width}x${height}` + path.extname(file.originalname);
+  sharp(file.path)
+  .resize(width, height)
+  .toFile(outputPath)
+  return outputPath;
+}
+
+export default {
+  resizeImage
+}
--- src/service/image.service.ts
+++ src/service/image.service.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from 'express';
 import multer from 'multer';
-import sharp from 'sharp';
-import path from 'path';
 import db from '../functions/db';
+import resize from '../functions/resize';
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -33,16 +32,8 @@ const saveImage = async (req: Request, res: Response) => {
         return res.send(err);
     }
 
-    const mediumThumbnailPath = './build/src/public/thumbnails/' + path.parse(req.file.originalname).name + '-resized-200x200' + path.extname(req.file.originalname);
-    const smallThumbnailPath = './build/src/public/thumbnails/' + path.parse(req.file.originalname).name + '-resized-100x100' + path.extname(req.file.originalname);
-
-    sharp(req.file.path)
-      .resize(200, 200)
-      .toFile(mediumThumbnailPath)
-
-    sharp(req.file.path)
-      .resize(100, 100)
-      .toFile(smallThumbnailPath)
+    const mediumThumbnailPath = resize.resizeImage(req.file, 200, 200)
+    const smallThumbnailPath = resize.resizeImage(req.file, 100, 100)
 
     try {
       const url = req.file.destination + req.file.originalname;
