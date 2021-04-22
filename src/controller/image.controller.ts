e7ec868dd36b79e610a9586ddb9533395e9d28fc
raphaelpg <raphael_pinto@outlook.com>
2021-04-22 15:26:00 +0100

    display api results in html file

--- src/controller/image.controller.ts
+++ src/controller/image.controller.ts
@@ -2,24 +2,21 @@ import { Request, Response } from 'express';
 import imageServices from '../service/image.service';
 
 const saveImage = async (req: Request, res: Response) => {
-  if (true) {
-    const result = await imageServices.saveImage(req, res);
-    return res.status(200).json({ msg: "posted image saved", response: result })
-  }
+  //control request here...
+  const result = await imageServices.saveImage(req, res);
+  return res.status(200).json({ msg: "posted image saved", response: result })
 };
 
 const getImageById = async (req: Request, res: Response) => {
-  if (true) {
-    const result = await imageServices.getImageById(req, res);
-    return res.status(200).json({ msg: "get image by id", response: result })
-  }
+  //control request here...
+  const result = await imageServices.getImageById(req, res);
+  return res.status(200).json({ msg: "get image by id", response: result })
 };
 
 const getAllImages = async (req: Request, res: Response) => {
-  if (true) {
-    const result = await imageServices.getImages();
-    return res.status(200).json({ msg: "get all images", response: result })
-  }
+  //control request here...
+  const result = await imageServices.getImages();
+  return res.status(200).json({ msg: "get all images", response: result })
 };
 
 export default {
